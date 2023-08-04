a74a9fdc338d76abaa8ba729ecc32ca88d7de501
miftahayuk <miftah.kusumawati@smma.co.id>
2023-08-04 15:15:21 +0700

    separate test file.py

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from main import *
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_add_negative():
+    assert add(-1, 1) == 0
+
+def test_add_strings():
+    result = add("Hello, ", "world!")
+    assert result == "Hello, world!"
